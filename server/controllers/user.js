bbbd76993be13d0316293e55c60ee269d4d774d4
Abantika Saha <abantika.saha18@gmail.com>
2022-01-14 01:08:48 +0530

    made model changes in controllers

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-import UserModal from "../models/user.js";
+import studentModel from "../models/studentModel.js";
 
 const secret = 'test';
 
@@ -10,7 +10,7 @@ export const signin = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const oldUser = await UserModal.findOne({ email });
+    const oldUser = await studentModel.findOne({ email });
 
     if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
 
@@ -31,13 +31,13 @@ export const signup = async (req, res) => {
   const { email, password, year , name } = req.body;
 
   try {
-    const oldUser = await UserModal.findOne({ email });
+    const oldUser = await studentModel.findOne({ email });
 
     if (oldUser) return res.status(400).json({ message: "User already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const result = await UserModal.create({ email, password: hashedPassword, name, year });
+    const result = await studentModel.create({ email, password: hashedPassword, name, year });
 
     const token = jwt.sign( { email: result.email, id: result._id }, secret, { expiresIn: "1h" } );
 
