f9e103f3b1069de171134e1aa87890f735542d6f
Madaka Purna Sai Prasanth <prasanthmps789@gmail.com>
2022-01-13 18:30:36 +0530

    added auth

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Home from './components/Home/Home'
+import Auth from './components/Auth/Auth'
+import Navbar from './components/Navbar/Navbar'
+
+
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Navbar/>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/auth" component={Auth} />  
+            </Switch>
+        
+        </BrowserRouter>
+    )
+}
+
+export default App;
