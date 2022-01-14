34781a6e9bed939e97305e8b211cd830432534b6
Madaka Purna Sai Prasanth <prasanthmps789@gmail.com>
2022-01-14 06:42:05 +0530

    added volunteer auth

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom'
 
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Home from './components/Home/Home'
-import Auth from './components/Auth/Auth'
+import AuthStudent from './components/AuthStudent/AuthStudent'
+import AuthVolunteer from './components/AuthVolunteer/AuthVolunteer'
 import Navbar from './components/Navbar/Navbar'
+import Auth from './components/Auth/Auth'
 
 
 
@@ -14,7 +16,9 @@ const App = () => {
             <Navbar/>
             <Switch>
                 <Route exact path="/" component={Home} />
-                <Route exact path="/auth" component={Auth} />  
+                <Route exact path="/auth" component={Auth} />
+                <Route exact path="/auth/student" component={AuthStudent} />  
+                <Route exact path="/auth/volunteer" component={AuthVolunteer} />  
             </Switch>
         
         </BrowserRouter>
